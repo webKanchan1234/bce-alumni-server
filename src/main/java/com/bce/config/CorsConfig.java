4dc38a4bc2878e871bcff06c700dff694d77f226
webKanchan1234 <kanchankr15153795@gmail.com>
2026-06-20 10:28:11 +0530

    update frontend url

--- src/main/java/com/bce/config/CorsConfig.java
+++ src/main/java/com/bce/config/CorsConfig.java
@@ -20,7 +20,8 @@ public class CorsConfig {
 
         config.setAllowedOrigins(
                 List.of(
-                        "http://localhost:5173"
+                        "http://localhost:5173",
+                        "https://famous-banoffee-7f5801.netlify.app"
                 )
         );
 
